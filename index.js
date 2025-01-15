056ea0c9809e324613e5d47bfb2b2e79a9d1c86c
WiliamAmorim <WiliamAmorim>
2025-01-14 22:45:58 -0300

    third commit

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 /** Utilizando API da https://www.alphavantage.co/ 
  *  git remote set-url origin git@github.com:WiliamAmorim/projeto1.git
+ *  git add index.js
+ *  git commit -m "second commit"
+ *  git push -u origin main
+ * https://www.youtube.com/watch?v=en1dycYtL48
 */
 require('dotenv').config();
 
@@ -34,6 +38,21 @@ app.get('/txcambio', async (req, res) => {
         res.status(500).json({ error: 'Erro ao buscar cotação' });
     }
 });
+
+app.get('/exchange_rate', async (req, res) => {
+    const symbol = req.query.symbol || 'BTC';
+    const url = `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${symbol}&to_currency=EUR&apikey=${process.env.APIKEY}`;
+    try {
+        const response = await axios.get(url);
+        const data = response.data;
+        res.json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erro ao buscar exchange rate' });
+    }
+});
+
+
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
 });
