e7af6860dcb78ced8573fbe9d5d24377dfc4d81e
WiliamAmorim <WiliamAmorim>
2025-02-02 18:16:34 -0300

    commit 3

--- index.js
+++ index.js
@@ -30,6 +30,19 @@ app.get('/txcambio', async (req, res) => {
     }
 });
 
+app.get('/exchange_rate', async (req, res) => {
+    const symbol = req.query.symbol || 'BTC';
+    const url = `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${symbol}&to_currency=EUR&apikey=${process.env.APIKEY}`;
+    try {
+        const response = await axios.get(url);
+        const data = response.data;
+        res.json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Erro ao buscar exchange rate' });
+    }
+});
+
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
 });
